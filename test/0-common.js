92ff9703ef4c2cca454a8ff4af4c08979502c856
Dany Laporte <dany_laporte@hotmail.com>
2016-09-07 04:46:56 -0400

    remove password from arango

--- test/0-common.js
+++ test/0-common.js
@@ -1,4 +1,4 @@
 global.expect = require('chai').expect;
 global.lock = require('../');
-global.server = require('arangojs')({ url: 'http://root:pass@127.0.0.1:8529' });
+global.server = require('arangojs')({ url: 'http://root@127.0.0.1:8529' });
 global.util = require('arango-util');
